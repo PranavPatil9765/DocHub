5ca86b8be73fcb56c06ab12f708ffb4c505eec8e
Pranav Patil <pranavpatil9765@gmail.com>
2026-02-08 11:59:14 +0530

    fixed pdf render issue

--- Frontend/DocHub/src/environments/environment.ts
+++ Frontend/DocHub/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiBaseUrl: 'https://dochub22.onrender.com'
-  apiBaseUrl: 'http://localhost:8080'
+  apiBaseUrl: 'https://dochub22.onrender.com'
+  // apiBaseUrl: 'http://localhost:8080'
 };
